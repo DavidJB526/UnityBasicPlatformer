b2d0f2a6ea2e3aca60a4bf8681c71ae694a1d5f4
DavidJB526 <david.beaudoin@loop.colum.edu>
2018-10-14 13:40:41 -0500

    Add Coin Spawning

---
+++ Assets/Scripts/SpawnCoins.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnCoins : MonoBehaviour {
+
+    [SerializeField]
+    private Transform[] coinSpawns;
+    [SerializeField]
+    private GameObject coin;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Spawn();
+	}
+	
+	private void Spawn()
+    {
+        for (int i = 0; i < coinSpawns.Length; i++)
+        {
+            int coinFlip = Random.Range(0, 2);
+            if (coinFlip > 0)
+            {
+                Instantiate(coin, coinSpawns[i].position, Quaternion.identity);
+            }
+        }
+    }
+}
