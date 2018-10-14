a73433d51da021a34c0260a8ac5ae91f9fa2d636
DavidJB526 <david.beaudoin@loop.colum.edu>
2018-10-14 13:27:41 -0500

    Add Falling Platforms

---
+++ Assets/Scripts/PlatformFall.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformFall : MonoBehaviour {
+
+    [SerializeField]
+    private float fallDelay;
+
+    private Rigidbody2D rb2d;
+
+	// Use this for initialization
+	void Awake ()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+	}
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Invoke("Fall", fallDelay);
+        }
+    }
+
+    private void Fall()
+    {
+        rb2d.isKinematic = false;
+    }
+}
